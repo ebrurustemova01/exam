a5e60371759ebac2b975162456f8929a278f5361
ebrurustemova01 <ebrurustemova44@gmail.com>
2022-03-18 11:03:59 -0700

    exam

---
+++ html17/app.js
@@ -0,0 +1,42 @@
+let h =[
+   ["A","B","C","D","E"],
+   ["Y","H","Q"],
+   ["P","D","Z","Y"]
+]
+
+let min=h[0][0]
+let c=[]
+let count = 0
+
+for(let i=0;i<h.length;i++){
+   for(let j=0;j<h[i].length;j++){
+    console.log(h[j][j])
+   }
+}
+
+
+
+
+// for(let i=0;i<h.length;i++){
+//    for(let j=0;j<h[i].length;j++){
+//     console.log(h[j])
+//    }
+
+// }
+
+// console.log(max)
+
+// for(i=0;i<h.length;i++){
+//    if(h[i]<min){
+//       min=h[i]
+//    }
+// }
+
+
+
+// // let x = "Coders"
+
+// for(let i = 0;i < h.length;i++){
+//     console.log(xh[i])
+// } 
+
